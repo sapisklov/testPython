ca7c18dd851058ac14568f3ac59103b678ed71fc
spisklov <sergei@pisklov.ru>
2023-03-18 15:36:12 +0500

    Добавил дополнительные вопросы

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 print('Привет! Это тестовая программа.')
-name=input("Как тебя зовут?\n")
+name = input("Как тебя зовут?\n")
 print("Привет,",name)
+years = int(input("Сколько тебе лет ?"))
+if years >= 18:
+    print(name, "тебе целых", years, "лет")
+elif years <= 10 and years >=5:
+    print(name, "тебе всего", years, "Лет ?")
+else:
+    print(name, 'Как ты пишешь ???')
